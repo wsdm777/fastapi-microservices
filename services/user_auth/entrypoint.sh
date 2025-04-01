1fdb5d0c309dd333a9549ea601c7292f1f1cbb2a
wsdm <kuznetsov06denis@yandex.ru>
2025-04-01 14:52:20 +0300

    add docker base

--- services/user_auth/entrypoint.sh
+++ services/user_auth/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Running migrations"
-alembic upgrade head
+poetry run alembic upgrade head
 
 UVICORN_WORKERS=${UVICORN_WORKERS:-1}
 
 echo "Starting application with $UVICORN_WORKERS workers..."
-exec gunicorn main:app --workers "$UVICORN_WORKERS" --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+exec poetry run gunicorn main:app --workers "$UVICORN_WORKERS" --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
