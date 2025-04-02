49402e473be6c090d686b1bcf2e37cdd089ea615
wsdm <kuznetsov06denis@yandex.ru>
2025-04-02 22:59:45 +0300

    create user repository

--- services/user_auth/user/repository.py
+++ services/user_auth/user/repository.py
@@ -1,32 +1,38 @@
 from sqlalchemy import select, text
 from sqlalchemy.ext.asyncio import AsyncSession
-from database.models import Rank, User
+
+from database.models import User
 from sqlalchemy.orm import joinedload
 
 
 class BaseRepository:
-    def __init__(self, session: AsyncSession, schema="user_auth"):
+    def __init__(self, session: AsyncSession):
         self.session = session
-        self.schema = schema
-        self._set_schema()
-
-    async def _set_schema(self):
-        query = text("SET search_path TO :schema").bindparams(schema=self.schema)
-        await self.session.execute(query)
-
-    async def add(self, obj):
-        await self.session.add(obj)
-
-    async def delete(self, obj):
-        await self.session.delete(obj)
 
 
 class UserRepository(BaseRepository):
-    def __init__(self, session: AsyncSession, schema):
-        super().__init__(session, schema)
+    def __init__(self, session: AsyncSession):
+        super().__init__(session)
 
-    async def get_by_id(self, id, load_related=False):
-        query = select(User).filter(User.id == id)
+    async def get_user(
+        self,
+        id: int | None = None,
+        login: str | None = None,
+        load_related: bool = False,
+    ) -> User | None:
+        query = select(User)
+        if not login and not id:
+            raise ValueError("Enter id or login")
+        if id:
+            query = query.filter(User.id == id)
+        if login:
+            query = query.filter(User.login == login)
         if load_related:
             query = query.options(joinedload(User.rank))
-        return await self.session.scalars(query).one_or_none()
+        user = await self.session.scalars(query)
+        return user.one_or_none()
+
+    async def get_user_password(self, login: str) -> str | None:
+        query = select(User.password_hash).filter(User.login == login)
+        password = await self.session.scalars(query)
+        return password.one_or_none()
