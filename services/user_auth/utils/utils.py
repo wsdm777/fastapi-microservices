ed50e533a6a272361840307a916380f6d0525124
wsdm <kuznetsov06denis@yandex.ru>
2025-04-02 22:59:32 +0300

    add jwt

--- services/user_auth/utils/utils.py
+++ services/user_auth/utils/utils.py
@@ -1,7 +1,11 @@
+from datetime import datetime, timedelta, timezone
+import uuid
 import bcrypt
 from sqlalchemy import Connection, text
+import jwt
 
-from config import SUPERUSER_EMAIL, SUPERUSER_PASSWORD
+from config import JWT_PRIVATE, SUPERUSER_EMAIL, SUPERUSER_PASSWORD
+from database.models import ACCESS_TOKEN_MINUTES_TTL, REFRESH_TOKEN_DAY_TTL, User
 
 
 def create_superuser_if_not_exists(connection: Connection):
@@ -56,3 +60,27 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
+
+
+def generate_refresh_token(
+    user_id: int, expires_delta: timedelta = timedelta(days=REFRESH_TOKEN_DAY_TTL)
+) -> tuple[str:str]:
+    expire = datetime.now(timezone.utc) + expires_delta
+    jti = str(uuid.uuid4())
+    payload = {"exp": expire, "jti": jti, "sub": user_id}
+    token = jwt.encode(payload=payload, key=JWT_PRIVATE, algorithm="RS256")
+    return token, jti
+
+
+def generate_access_token(
+    user: User, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_MINUTES_TTL)
+) -> str:
+    expire = datetime.now(timezone.utc) + expires_delta
+    payload = {
+        "sub": user.id,
+        "login": user.login,
+        "level": user.rank.level,
+        "exp": expire,
+    }
+    token = jwt.encode(payload=payload, key=JWT_PRIVATE, algorithm="RS256")
+    return token
